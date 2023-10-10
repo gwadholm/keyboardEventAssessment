6601934c9c7929c458ed18bdcc87db89d6c78c68
gwadholm <gwadholm@gmail.com>
2023-10-10 12:51:08 -0400

    cleaned up comments

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-/*Set up a letter array and create variables for working the body and ol DOM elements*/
+/*Set up a letter array and create variables for working with the body and ol DOM elements*/
 const letterArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const body = document.querySelector('body');
 const list = document.querySelector('ol');
 
-/*Choosing the randomLetter and returning it to a variable to use in the key event */
+/*Choose the randomLetter and return it to a variable to use in the key event */
 const randomLetter = function() {
   return letterArray[Math.floor(Math.random() * 25)];
 };
 
-/*set initial secret Letter */
+/*Set initial secretLetter */
 let secretLetter = randomLetter();
 
- /*The key event that triggers the message*/
+ /*The key event that triggers the message and then creates a new secretLetter*/
  body.addEventListener('keyup', function(e){
   if (e.key === secretLetter) {
     const winnerMessage = document.createElement('li');
